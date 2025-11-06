732b80b5674ce73357e3d2ba2ffe08953adaed64
marc481 <marcusocampo1313@gmail.com>
2025-11-06 01:30:55 +0000

    Basic Inputs Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [totalSales, setTotalSales] = useState(``);
+  const [startTime, setStartTime] = useState("6:00");
+  const [endTime, setEndTime] = useState("14:00");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-500 text-white p-8">
+      <h1 className="text-4xl font-bold">Hourly Sales Calculator</h1>
+
+      <div>
+        <label> Total Sales</label>
+        <input
+          type="number"
+          value={totalSales}
+          onChange={(e) => setTotalSales(e.target.value)}
+          placeholder="Enter Total Sales"
+        />
+
+        <div>
+          <label>Enter Start Time</label>
+          <input
+            type="time"
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Enter End Time</label>
+          <input
+            type="time"
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
   );
 }
