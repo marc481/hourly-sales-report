95fd1baf95d0938a5647af6f37b817279bc8a622
marc481 <marcusocampo1313@gmail.com>
2025-11-09 22:37:22 +0000

    Learning Tailwind

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
 import { useState } from "react";
 
 function App() {
-  const [totalSales, setTotalSales] = useState(``);
+  const [totalSales, setTotalSales] = useState("");
   const [startTime, setStartTime] = useState("6:00");
   const [endTime, setEndTime] = useState("14:00");
 
   return (
     <div className="bg-blue-500 text-white p-8">
-      <h1 className="text-4xl font-bold">Hourly Sales Calculator</h1>
+      <h1 className="text-4xl font-bold mb-6">Hourly Sales Calculator</h1>
 
-      <div>
-        <label> Total Sales</label>
+      <div className="mb-4">
+        <label className="block mb-2 font-semibold">Total Sales</label>
         <input
           type="number"
           value={totalSales}
           onChange={(e) => setTotalSales(e.target.value)}
-          placeholder="Enter Total Sales"
+          className="w-full p-2 text-black rounded"
+          placeholder="Enter total sales"
         />
+      </div>
 
-        <div>
-          <label>Enter Start Time</label>
-          <input
-            type="time"
-            value={startTime}
-            onChange={(e) => setStartTime(e.target.value)}
-          />
-        </div>
+      <div className="mb-4">
+        <label className="block mb-2 font-semibold"> Enter Start Time</label>
+        <input
+          type="time"
+          value={startTime}
+          onChange={(e) => setStartTime(e.target.value)}
+          className="w-full p-2 text-black rounded"
+        />
+      </div>
 
-        <div>
-          <label>Enter End Time</label>
-          <input
-            type="time"
-            value={endTime}
-            onChange={(e) => setEndTime(e.target.value)}
-          />
-        </div>
+      <div className="mb-4">
+        <label className="block mb-2 font-semibold"> Enter End Time</label>
+        <input
+          type="time"
+          value={endTime}
+          onChange={(e) => setEndTime(e.target.value)}
+          className="w-full p-2 text-black rounded"
+        />
       </div>
     </div>
   );
 }
-
 export default App;
